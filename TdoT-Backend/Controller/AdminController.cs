d7b46701a74225a305deadc62a3115ee4c35d58b
Stefan456789 <stefanfarbe@gmail.com>
2025-02-21 15:25:32 +0100

    Add placeholder

--- TdoT-Backend/Controller/AdminController.cs
+++ TdoT-Backend/Controller/AdminController.cs
@@ -1,31 +1,44 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TdoT_Backend.Dtos;
 using TdoT_Backend.Services;
 
 namespace TdoT_Backend.Controller;
 
 [Authorize(AuthenticationSchemes = "AdminScheme")]
 [ApiController]
-public class AdminController(AdminService service) : ControllerBase
+public class AdminController(AdminService adminService, DataService dataService) : ControllerBase
 {
     [HttpGet("files")]
     public IActionResult GetFiles(string? fileName)
     {
         if (fileName != null)
         {
-            var file = service.GetFile(fileName);
+            var file = adminService.GetFile(fileName);
             if (file.Length == 0)
             {
                 return NotFound();
             }
             return File(file, "text/plain", Path.GetFileName(fileName));
         }
-        return Ok(service.GetFiles());
+        return Ok(adminService.GetFiles());
     }
 
     [HttpPost("files")]
     public IActionResult PostFiles(IFormFile file, string? fileName)
     {
-        return service.PostFile(file.OpenReadStream(), fileName ?? file.FileName) ? Ok() : BadRequest();
+        return adminService.PostFile(file.OpenReadStream(), fileName ?? file.FileName) ? Ok() : BadRequest();
+    }
+
+    [HttpGet("placeholder")]
+    public IActionResult GetPlaceholders()
+    {
+        return Ok(dataService.GetPlaceholders() ?? throw new FileNotFoundException());
+    }
+
+    [HttpPut("placeholder")]
+    public IActionResult PutPlaceholders(PlaceholderDto[] placeholder)
+    {
+        return Ok(adminService.PutPlaceholders(placeholder));
     }
 }
