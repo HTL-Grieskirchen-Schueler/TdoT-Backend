6b18fe363d3e9a23f08405ff542b366e150b778b
Stefan456789 <stefanfarbe@gmail.com>
2025-02-22 19:44:03 +0100

    Hotfix: Add room to activity

--- TdoT-Backend/Dtos/ActivityDto.cs
+++ TdoT-Backend/Dtos/ActivityDto.cs
@@ -3,4 +3,5 @@ public class ActivityDto
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public string Room { get; set; } = string.Empty;
 }
