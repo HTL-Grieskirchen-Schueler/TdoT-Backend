2c4c17b753327aa3834127a6a88e820d53da24db
Stefan456789 <stefanfarbe@gmail.com>
2025-03-08 21:15:09 +0100

    Hotfix node story

--- TdoT-Backend/Dtos/NodeDto.cs
+++ TdoT-Backend/Dtos/NodeDto.cs
@@ -3,6 +3,7 @@ public class NodeDto
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public int Story { get; set; }
     public string Width { get; set; } = string.Empty;
     public string Height { get; set; } = string.Empty;
 }
